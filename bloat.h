b27495e0dce9573e9e2335b19daae359731cfca4
Nilso Júnior <162613094+nilsojunior@users.noreply.github.com>
2026-04-16 10:41:45 -0300

    [bloat] Add types

--- bloat.h
+++ bloat.h
@@ -21,6 +21,16 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
 #define DEFER(value) do { result = (value); goto defer; } while(0)
 
 #define SIZEOF(array) sizeof((array)) / sizeof((array[0]))
